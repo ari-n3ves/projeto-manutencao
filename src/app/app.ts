541baa3cc053fd69d51343e16dd646bc3198939f
Ysis Barduco <ysisbarduco@gmail.com>
2026-08-20 00:06:16 -0300

    Estrutura da página para visulização do orçamento

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DadosSolicitacao } from './pages/dados-solicitacao';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DadosSolicitacao],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
