8d1da212d7fc737b9f117dd17043d7319a84d934
Ana Clara Rozin <anarozin002@gmail.com>
2026-08-26 23:50:21 -0300

    Melhora layout da tela inicial do cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
+
 import { Inicio } from './cliente/inicio/inicio';
+import { OrcamentoClien } from './cli/orcamento-clien/orcamento-clien';
+import { OrcamentoFunc } from './features/orcamento-func/orcamento-func';
+import { DadosSolicitacao } from './pages/dados-solicitacao/dados-solicitacao';
 
 export const routes: Routes = [
   {
     path: 'cliente/inicio',
     component: Inicio
+  },
+  {
+    path: 'orcamento-cliente',
+    component: OrcamentoClien
+  },
+  {
+    path: 'orcamento-funcionario',
+    component: OrcamentoFunc
+  },
+  {
+    path: 'dados-solicitacao',
+    component: DadosSolicitacao
   }
 ];
